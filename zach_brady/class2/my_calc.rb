ccf553ba4711d864b03a8198cc68dc8e2db211ce
Zach Brady <zacbra@gmail.com>
2015-11-05 21:17:39 -0500

    Create subtract method

--- zach_brady/class2/my_calc.rb
+++ zach_brady/class2/my_calc.rb
@@ -1,10 +1,16 @@
 puts "Give me the first number: "
 num1 = gets.chomp.to_i
 
-def add(num)
+puts "Give me the second number: "
+num2 = gets.chomp.to_i
 
-    num + 4
-
+def add(num, other_num)
+    num + other_num
 end
 
-puts "The answer is #{add(num1)}"
+def subtract(num, other_num)
+    num - other_num
+end
+
+puts "#{num1} added to #{num2} is #{add(num1, num2)}"
+puts "#{num1} minus #{num2} is #{subtract(num1, num2)}"
