9b109303ed65b0e8355e7031af825a036f3f5faa
Judith Shimer <thesneakymister@gmail.com>
2015-11-07 11:37:17 -0500

    uploaded homework from class2

--- judith_shimer/class2/bouncer.rb
+++ judith_shimer/class2/bouncer.rb
@@ -1,10 +1,14 @@
 puts "How old are you?"
 age = gets.chomp.to_i
 
-  if age == 21
+def check_age(a)
+  if a == 21
   	puts "Just old enough!"
-  elsif age < 21
+  elsif a < 21
   	puts "Get out of here!"
   else
   	puts "Come on in!"
   end
+end
+
+print check_age(age)
