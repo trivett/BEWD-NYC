0c76856675d5f34f6298dd3448ecce2f56560e6d
Zach Brady <zacbra@gmail.com>
2015-11-08 17:51:03 -0500

    Wrap the age checking logic in method check_age

--- zach_brady/class2/bouncer.rb
+++ zach_brady/class2/bouncer.rb
@@ -1,13 +1,15 @@
-puts "what is your age"
+puts "what is your age?"
 
 age = gets.chomp.to_i
 
-
-if age == 21
-    puts "just old enough"
-elsif age < 21
-    puts "toooooo young. get outta here."
-else
-    puts "come on in"
+def check_age(age)
+    if age == 21
+        puts "just old enough"
+    elsif age < 21
+        puts "toooooo young. get outta here."
+    else
+        puts "come on in"
+    end
 end
 
+check_age(age)
