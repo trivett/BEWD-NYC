c248ac5d43b4d4285622a77844913adbfe2db0f6
Zach Brady <zacbra@gmail.com>
2015-11-08 18:24:31 -0500

    Extend my_calc with other functions

--- zach_brady/class2/my_calc.rb
+++ zach_brady/class2/my_calc.rb
@@ -1,3 +1,12 @@
+puts "What would you like to do? ('add', 'subtract', 'multiply', 'divide')"
+operation = gets.chomp
+
+valid_operations = ['add', 'subtract', 'multiply', 'divide']
+
+if !valid_operations.include? operation
+    puts "Not a valid operation"
+end
+
 puts "Give me the first number: "
 num1 = gets.chomp.to_i
 
@@ -12,5 +21,21 @@ def subtract(num, other_num)
     num - other_num
 end
 
-puts "#{num1} added to #{num2} is #{add(num1, num2)}"
-puts "#{num1} minus #{num2} is #{subtract(num1, num2)}"
+def multiply(num, other_num)
+    num * other_num
+end
+
+def divide(num, other_num)
+    num / other_num
+end
+
+case operation
+when 'add'
+    puts add(num1, num2)
+when 'subtract'
+    puts subtract(num1, num2)
+when 'multiply'
+    puts multiply(num1, num2)
+when 'divide'
+    puts divide(num1, num2)
+end
