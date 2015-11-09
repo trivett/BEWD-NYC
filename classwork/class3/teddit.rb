b846cdbac8eb0e06fc75b85639c4e188892b67fb
Vincent Trivett <trivett@gmail.com>
2015-11-09 18:53:41 -0500

    teddit classwork

---
+++ classwork/class3/teddit.rb
@@ -0,0 +1,20 @@
+# I'm sure we have all noticed that on sites like Reddit and Twitter, anything involving a cat seems to get a lot of attention.
+# This program will prompt the user for a headline, scan the headline for the word "cat" and the user will and automatically assign fake upvotes to the story if there is the mention of a cat.
+
+
+# prompt the user to enter a headline
+
+
+# save the headline as a variable and "gets.chomp" the headline from the user
+
+# set an upvotes variable. Where should the number of upvotes start? 
+
+
+
+
+
+# define a method that scans for the word cat, and pass in the headline that the user gave you, and the upvotes variable.
+
+# call the method so something happens!
+
+# print to the screen the story and number of upvotes. 
---
+++ classwork/class3/teddit_hash.rb
@@ -0,0 +1,12 @@
+# Let's make an array of hashes to represent news stories in Teddit.
+
+#first, literally write three hashes of stories into a news array. Each story hash should have headline, category, and upvotes
+
+# prompt the user to enter a new story headline, then a category, and then number of upvotes.
+
+# push the new story into the news array.
+
+#iterate over the news array and print each story with the following format: 
+# Headline: Obama Invites Pizza Rat to White House
+# Category: Politics
+# Upvotes: 90
