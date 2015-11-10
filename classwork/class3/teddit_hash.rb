096b01d5da235b634e3d8c63ca86c0408fa1e2be
Vincent Trivett <trivett@gmail.com>
2015-11-09 19:34:07 -0500

    regular teddit in the right place

--- classwork/class3/teddit_hash.rb
+++ classwork/class3/teddit_hash.rb
@@ -1,10 +1,13 @@
 # Let's make an array of hashes to represent news stories in Teddit.
 
+
+
 # #first, literally write three hashes of stories into a news array. Each story
 # hash should have headline, category, and upvotes. If you want some
 # inspiration, go for The Onion or something. 
 
 
+#create a hash that saves each of these values
 
 # # prompt the user to enter a new story headline, then a category, and then
 # number of upvotes.
@@ -16,14 +19,16 @@
 # Category: Politics
 # Upvotes: 90
 
+# print_story(news_story)
+
+
+
+#
 # Call the method on the variable that saves the user-submitted story
 
-
 # ================== step two ================
-
 # Push the hash of the user-created story into the array you defined earlier. You
 # can use either the .push method or that awesome shovel << method to do it.
-
 
 # let's iterate over the array of hashes and pass each (hint hint) one into the
 # method that prints them out all pretty.
