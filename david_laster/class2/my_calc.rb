83c126f02b86c06990acf6b0aa6562b8c96bc2e7
David Laster <lasterd@gmail.com>
2015-11-10 17:56:18 -0500

    homework done

--- david_laster/class2/my_calc.rb
+++ david_laster/class2/my_calc.rb
@@ -1,6 +1,8 @@
-puts "give me the first number"
+puts "What would you like to do? ('add', 'subtract', 'multiply', 'divide')?"
+operation = gets.chomp
+puts "enter the first number"
 num1 = gets.chomp.to_i
-puts "give me the second number"
+puts "enter the second number"
 num2 = gets.chomp.to_i
 
 def add(x,y)
@@ -11,4 +13,32 @@ def subtract(x,y)
 	x - y
 end
 
-puts "the answer, added, is #{add(num1,num2)}, and subtracted is #{subtract(num1,num2)}"
+def multiply(x,y)
+	x * y
+end
+
+def divide(x,y)
+	x / y
+end
+
+def modular(x,y)
+	x % y
+end
+
+def calc(operation, num1, num2)
+if operation == "add"
+	add(num1,num2)
+elsif operation == "subtract"
+	subtract(num1,num2)
+elsif operation == "multiply"
+	multiply(num1,num2)
+elsif operation == "divide"
+	divide(num1,num2)
+else
+	"unknown without a valid operation"
+end
+end
+
+result = calc(operation, num1, num2)
+
+puts "the answer is #{result}"
