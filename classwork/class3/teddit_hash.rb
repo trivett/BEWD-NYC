60826efd38be3cb4882b59e080b361e60d6fcb32
Vincent Trivett <trivett@gmail.com>
2015-11-09 19:14:47 -0500

    updated teddit hash exercise

--- classwork/class3/teddit_hash.rb
+++ classwork/class3/teddit_hash.rb
@@ -1,12 +1,30 @@
 # Let's make an array of hashes to represent news stories in Teddit.
 
-#first, literally write three hashes of stories into a news array. Each story hash should have headline, category, and upvotes
+# #first, literally write three hashes of stories into a news array. Each story
+# hash should have headline, category, and upvotes. If you want some
+# inspiration, go for The Onion or something. 
 
-# prompt the user to enter a new story headline, then a category, and then number of upvotes.
 
-# push the new story into the news array.
 
-#iterate over the news array and print each story with the following format: 
+# # prompt the user to enter a new story headline, then a category, and then
+# number of upvotes.
+
+
+# make a method that accepts a story as a parameter.
+# The method should print each story with the following format: 
 # Headline: Obama Invites Pizza Rat to White House
 # Category: Politics
 # Upvotes: 90
+
+# Call the method on the variable that saves the user-submitted story
+
+
+# ================== step two ================
+
+# Push the hash of the user-created story into the array you defined earlier. You
+# can use either the .push method or that awesome shovel << method to do it.
+
+
+# let's iterate over the array of hashes and pass each (hint hint) one into the
+# method that prints them out all pretty.
+
