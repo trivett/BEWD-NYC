969f2e81aaaab52354d397172d94db2ed428a244
Zach Brady <zacbra@gmail.com>
2015-11-12 19:52:51 -0500

    Create dictionary of animals and their plural form

--- zach_brady/class4/music.rb
+++ zach_brady/class4/music.rb
@@ -1,10 +1,12 @@
-musicians = [
+contemporary_musicians = [
     "Prince",
     "Vince Staples",
     "Mac Demarco",
     "Oneohtrix Point Never",
     "Nikki Minaj"
 ]
+
+
 
 musicians.each do |m|
     puts m.reverse
