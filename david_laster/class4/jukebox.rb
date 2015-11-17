d0be6acc0051f7fb1e4a394b65eb8f3aabdba15a
David Laster <lasterd@gmail.com>
2015-11-17 16:38:58 -0500

    homework

--- david_laster/class4/jukebox.rb
+++ david_laster/class4/jukebox.rb
@@ -1,8 +1,11 @@
 require 'httparty'
 require 'json'
-artist = "santell"
-j = HTTParty.get('https://itunes.apple.com/search?term='+artist+'&limit=5')
+artist = puts
+j = JSON.parse(HTTParty.get('https://itunes.apple.com/search?term='+artist+'&limit=5'))
+
 j["results"].each do |song|
 puts song["trackName"] + " " + song["previewUrl"]
 end
 
+
+j = JSON.parse(response)
