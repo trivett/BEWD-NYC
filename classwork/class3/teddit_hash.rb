67a6438d9a11afabd3c5a76fb352a9324c126417
Christine Zhu <christine.y.zhu@gmail.com>
2015-11-16 19:36:34 -0500

    teddits

--- classwork/class3/teddit_hash.rb
+++ classwork/class3/teddit_hash.rb
@@ -1,5 +1,22 @@
 # Let's make an array of hashes to represent news stories in Teddit.
 
+story1 = {
+	"headline" => "Obama Invites Pizza Rat to White House",
+	"category" => "Politics",
+	"upvotes" => 90
+}
+
+story2 = {
+	"headline" => "Cat eats dog in Brooklyn",
+	"category" => "NYC",
+	"upvotes" => 10
+}
+
+story3 = {
+	"headline" => "Madonna pledges to stop making music videos",
+	"category" => "Arts",
+	"upvotes" => 55,
+}
 
 
 # #first, literally write three hashes of stories into a news array. Each story
@@ -12,6 +29,29 @@
 # # prompt the user to enter a new story headline, then a category, and then
 # number of upvotes.
 
+user_story = {}
+
+puts "Create a headline"
+user_story["headline"] = gets.chomp
+
+puts "Select a category"
+user_story["category"] = gets.chomp
+
+puts "How many upvotes?"
+user_story["upvotes"] = gets.chomp.to_i
+
+puts user_story
+
+def print_story(story)
+
+puts "Headline: #{story["headline"]}"
+puts "Category: #{story["category"]}"
+puts "Upvotes: #{story["upvotes"]}"
+puts "================================"
+
+end
+
+print_story(user_story)
 
 # make a method that accepts a story as a parameter.
 # The method should print each story with the following format: 
@@ -20,6 +60,8 @@
 # Upvotes: 90
 
 # print_story(news_story)
+
+
 
 
 
@@ -32,4 +74,31 @@
 
 # let's iterate over the array of hashes and pass each (hint hint) one into the
 # method that prints them out all pretty.
+
+
+
+todays_news = []
+
+todays_news << story1
+todays_news << story2
+todays_news << story3
+todays_news << user_story
+
+puts todays_news
+
+
+
+todays_news.each do |story| #prints out the story each time so you don't have to write it out multiple times.
+	print_story(story)
+end
+
+
+
+
+
+
+
+
+
+
 
