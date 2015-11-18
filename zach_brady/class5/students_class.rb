6c6f4f36875a242f7b63d996f0cf7fda2f6140cb
Zach Brady <zacbra@gmail.com>
2015-11-17 19:36:09 -0500

    Update to puts out a list of students

--- zach_brady/class5/students_class.rb
+++ zach_brady/class5/students_class.rb
@@ -1,13 +1,24 @@
 class Student
     attr_accessor :name, :gpa, :major
+
+    def initialize(name, gpa, major)
+        @name = name
+        @gpa = gpa
+        @major = major
+    end
+
+    def to_s
+        "#{ @name } majors in #{ @major }. GPA: #{ @gpa }."
+    end
 end
 
-alice = Student.new
 
-alice.name = "Alice"
-alice.gpa = "A"
-alice.major = "Math"
+alice = Student.new("Alice", "A", "Math")
+bob = Student.new("Bob", "B", "Lit")
+carlos = Student.new("Carlos", "C", "Chemistry")
 
-puts alice.name
-puts alice.gpa
-puts alice.major
+students = [alice, bob, carlos]
+
+students.each do |student|
+    puts student
+end
