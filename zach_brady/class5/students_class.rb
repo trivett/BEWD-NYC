bfaca030bb436da792e40aed23bf4cdb52db90f8
Zach Brady <zacbra@gmail.com>
2015-11-17 19:53:27 -0500

    Add aging method to Student class

--- zach_brady/class5/students_class.rb
+++ zach_brady/class5/students_class.rb
@@ -1,24 +1,40 @@
 class Student
-    attr_accessor :name, :gpa, :major
+    attr_accessor :name, :gpa, :major, :age
 
-    def initialize(name, gpa, major)
+    def initialize(name, gpa, major, age)
         @name = name
         @gpa = gpa
         @major = major
+        @age = age
     end
 
     def to_s
-        "#{ @name } majors in #{ @major }. GPA: #{ @gpa }."
+        "#{ @name } majors in #{ @major }. GPA: #{ @gpa }. They are #{ @age } years old."
+    end
+
+    def passing?
+        if @gpa > "D"
+            return false
+        else 
+            return true
+        end
+    end
+
+    def another_year_gone
+        @age= @age + 1
     end
 end
 
+alice = Student.new("Alice", "A", "Math", 18)
+bob = Student.new("Bob", "B", "Lit", 20)
+carlos = Student.new("Carlos", "C", "Chemistry", 23)
 
-alice = Student.new("Alice", "A", "Math")
-bob = Student.new("Bob", "B", "Lit")
-carlos = Student.new("Carlos", "C", "Chemistry")
+# students = [alice, bob, carlos]
 
-students = [alice, bob, carlos]
+# students.each do |student|
+#     puts student
+# end
 
-students.each do |student|
-    puts student
-end
+puts bob
+bob.another_year_gone
+puts bob
