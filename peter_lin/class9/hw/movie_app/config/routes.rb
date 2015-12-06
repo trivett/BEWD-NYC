fb00025c6206578e4bbbe4c6315f2f5b160fc022
Peter Lin <peterlin741@gmail.com>
2015-12-06 18:23:06 -0500

    changes routes

--- peter_lin/class9/hw/movie_app/config/routes.rb
+++ peter_lin/class9/hw/movie_app/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   root 'movies#index'
   get '/movies' => 'movies#index' 
+  get '/:id' => 'movies#show' 
   get '/movies/:id' => 'movies#show' 
 
   # The priority is based upon order of creation: first created -> highest priority.
