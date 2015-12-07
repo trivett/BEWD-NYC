8d867e95cd902e91bb835d666d2b4db8fea6432c
Christine Zhu <christine.y.zhu@gmail.com>
2015-12-06 21:13:27 -0500

    rockpaperscissors and movie_app hw

--- christine_zhu/class9/movie_app/app/controllers/movies_controller.rb
+++ christine_zhu/class9/movie_app/app/controllers/movies_controller.rb
@@ -1,6 +1,14 @@
 class MoviesController < ApplicationController
 
 	def index
+
+		@movies = Movie.all
+
+	end
+
+	def show
+		@movie = Movie.find(params[:id])
+
 	end
 
 end
--- christine_zhu/class9/movie_app/db/seeds.rb
+++ christine_zhu/class9/movie_app/db/seeds.rb
@@ -1,3 +1,13 @@
+Movie.create(title: "50 First Dates", description: "romcom set in Hawaii where a girl has no long term memory and can only remember one day.", year_released: 2004)
+
+Movie.create(title: "Harry Potter and the Sorcerer's Stone", description: "Young wizards.", year_released: 2001)
+
+
+
+
+
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
