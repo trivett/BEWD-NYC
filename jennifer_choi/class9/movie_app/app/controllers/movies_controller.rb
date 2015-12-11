7d50574aa8d5cd26ea06a13663c81a62572b9129
Jennifer <fashed@gmail.com>
2015-12-10 21:29:53 -0500

    Updated Movie App in Class 9 folder

--- jennifer_choi/class9/movie_app/app/controllers/movies_controller.rb
+++ jennifer_choi/class9/movie_app/app/controllers/movies_controller.rb
@@ -5,4 +5,30 @@ class MoviesController < ApplicationController
 	def show
 		@movie = Movie.find(params[:id])
 	end
+
+	def destroy
+		Movie.find(params[:id]).destroy
+        redirect_to movies_path
+	end
+
+	def new
+		@movie = Movie.new
+	end
+
+	def create
+		@movie = Movie.new(safe_movie_params)
+		if @movie.save
+			flash[:notice] = "Movie created!"
+			redirect_to movies_path
+		else
+			flash[:notice] = "Movie not created"
+			redirect_to new_movie_path
+		end
+	end
+
+	private
+
+	def safe_movie_params
+		 params.require(:movie).permit(:title, :description, :year_released)
+	end
 end
