bc2a43f170dacb8201f219d59b64900f3c40e2e6
Peter Lin <peterlin741@gmail.com>
2015-12-11 01:46:49 -0500

    working on movies app

--- peter_lin/class9/hw/movie_app/app/controllers/movies_controller.rb
+++ peter_lin/class9/hw/movie_app/app/controllers/movies_controller.rb
@@ -9,4 +9,17 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
   end
 
+  def destroy
+    Movie.find(params[:id]).delete
+    redirect_to movies_path
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    # safe_product_params = params.require(:movie).require()
+  end
+
 end
--- peter_lin/class9/hw/movie_app/config/routes.rb
+++ peter_lin/class9/hw/movie_app/config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
 
 
   root 'movies#index'
-  get '/movies' => 'movies#index' 
-  get '/:id' => 'movies#show' 
-  get '/movies/:id' => 'movies#show' 
+  # get '/movies' => 'movies#index' 
+  # get '/:id' => 'movies#show' 
+  # get '/movies/:id' => 'movies#show' 
+
+  resources :movies
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
