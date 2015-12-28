74db2ad78ec9c7f55ec7e4867bcfe7e3bcf3dfab
Peter Lin <peterlin741@gmail.com>
2015-12-28 03:05:31 -0500

    finished class13 ga app hw

--- peter_lin/class13/general_assembly/app/controllers/instructors_controller.rb
+++ peter_lin/class13/general_assembly/app/controllers/instructors_controller.rb
@@ -1,32 +1,64 @@
-# class InstructorsController < ApplicationController
-#     def new
-#       @course = Course.find(params[:course_id])
-#       @instructor = @course.instructor.new
-#     end
+class InstructorsController < ApplicationController
 
-#     def create
-#       @course = Course.find(params[:course_id])
-#       @instructor = @course.instructor.new(instructor_params)
+    def show
+      @course = Course.find(params[:course_id])
+      @instructor = @course.instructor
+    end
 
-#       if @instructor.save
-#         flash[:notice] = "#{ @instructor.name } has joined #{ @course.name } as an instructor!"
-#         redirect_to course_students_path
-#       else
-#         flash[:notice] = "New instructor could not be created. Try again."
-#         redirect_to new_course_instructor_path
-#       end
-#     end
+    def new
+      @course = Course.find(params[:course_id])
+      @instructor = Instructor.new
+      # @course.instructor = @instructor
 
-#     def destroy
-#       flash[:notice] = "Student deleted!"
-#       Instructor.find(params[:id]).destroy
-#       redirect_to course_students_path
-#     end
+    end
+
+    def create
+      @course = Course.find(params[:course_id])
+      @instructor = Instructor.new(instructor_params)
+    
+
+      if @instructor.save
+
+        @course.instructor = @instructor
+
+        flash[:notice] = "#{ @instructor.name } has joined #{ @course.name } as an instructor!"
+        redirect_to course_students_path
+      else
+        flash[:notice] = "New instructor could not be created. Try again."
+        redirect_to new_course_instructor_path
+      end
+    end
+
+    def destroy
+      flash[:notice] = "Instructor deleted!"
+      Instructor.find(params[:id]).destroy
+      redirect_to course_students_path
+    end
+
+    def edit
+      @course = Course.find(params[:course_id])
+      @instructor = Instructor.find(params[:id])
+    end
+
+    def update
+      @course = Course.find(params[:course_id])
+      @instructor = Instructor.find(params[:id])
+      if @instructor.update(instructor_params)
+        flash[:notice] = "Instructor Updated"
+        redirect_to course_students_path
+      else
+        flash[:notice] = "Instructor Update Failed"
+        redirect_to edit_course_instructor_path
+        
+        # Render doesn't work ?
+        # render edit_course_instructor_path
+      end
+    end
       
-#   private
+  private
 
-#   def instructor_params
-#     params.require(:instructor).permit(:name)
-#   end
+  def instructor_params
+    params.require(:instructor).permit(:name)
+  end
 
-# end
+end
